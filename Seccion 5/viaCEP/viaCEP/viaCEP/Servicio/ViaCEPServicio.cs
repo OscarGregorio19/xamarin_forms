b5e392cb89e81a604a5742ef7a49d2d2612ba3e6
Oscar <oscar.gregorio19@gmail.com>
2018-10-01 18:51:17 -0500

    seccion 5 empezando

---
+++ Seccion 5/viaCEP/viaCEP/viaCEP/Servicio/ViaCEPServicio.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using viaCEP.Modelo;
+
+namespace viaCEP.Servicio
+{
+    public class ViaCEPServicio
+    {
+        private static string EndecoreURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static object Conteudo { get; private set; }
+
+        public static Endereco BuscarEndecoreViaCEP(string cep)
+        {
+            string nuevaURL = string.Format(EndecoreURL, cep);
+
+            WebClient wc = new WebClient();
+            string contenido = wc.DownloadString(nuevaURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(contenido);
+
+            return end;
+        }
+    }
+}
+
